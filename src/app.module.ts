035584bd2f4b2d32b2d662efcfbc427e1cc92576
Марченко Александр Александрович <aamarc@mts.ru>
2021-07-25 19:54:20 +0300

    feat: подключен orm и создан модуль User с entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NoteModule } from './note/note.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entity/user.entity';
 
 @Module({
-  imports: [NoteModule],
+  imports: [
+    NoteModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'aamarc26',
+      password: '',
+      database: 'noteself',
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
