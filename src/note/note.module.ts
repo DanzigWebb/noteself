436818d240f3b9973fc6b171c57d407e3fc766d2
Марченко Александр Александрович <aamarc@mts.ru>
2021-07-25 22:04:16 +0300

    refactor: экспорт userService из userModule

--- src/note/note.module.ts
+++ src/note/note.module.ts
@@ -3,13 +3,12 @@ import { NoteController } from './note.controller';
 import { NoteService } from './note.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Note } from './entity/note.entity';
-import { UserService } from '../user/user.service';
 import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Note]), UserModule],
   controllers: [NoteController],
-  providers: [NoteService, UserService],
+  providers: [NoteService],
   exports: [TypeOrmModule],
 })
 export class NoteModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -8,6 +8,6 @@ import { User } from './entity/user.entity';
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
-  exports: [TypeOrmModule],
+  exports: [TypeOrmModule, UserService],
 })
 export class UserModule {}
